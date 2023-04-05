37cc02943b4de9226516c43835732d9b0a6fda6a
Kirill Iost <kirilliost13@gmail.com>
2023-04-05 18:14:15 +0600

    Add support for VK

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,12 @@ from decouple import config
 from os import environ
 import json
 
-def deserialize(json):
+def deserialize_vk(json):
+    chat_id = json['object']['message']['from_id']
+    text = json['object']['message']['text']
+    return chat_id, text
+
+def deserialize_tg(json):
     chat_id = json['message']['chat']['id']
     text = json['message']['text']
     return chat_id, text
@@ -15,12 +20,18 @@ def main_server_request(message):
             }
     return requests.post(config('MAIN_SERVER_ADDRESS'), json=payload)
                  
-def send_message(payload):
+def send_message_vk(payload):
+    url = "https://api.vk.com/method/messages.send"
+    params = payload 
+    response = requests.post(url, params=params)
+    return response.json()
+
+def send_message_tg(payload):
     token = config('TOKEN')
     url = f'https://api.telegram.org/bot{token}/sendMessage'
     return requests.post(url, json=payload)
 
-def generate_payload(chat_id, text=None, widgets=None):
+def generate_payload_tg(chat_id, text=None, widgets=None):
     payload = {
             "parse_mode": "HTML",
             "chat_id": chat_id
@@ -28,5 +39,15 @@ def generate_payload(chat_id, text=None, widgets=None):
     
     if(text):
         payload['text'] = text
+
+    return payload
+
+def generate_payload_vk(chat_id, text):
+    payload = {
+            "access_token": config('ACCESS_TOKEN'),
+            "v": 5.131,
+            "peer_id": chat_id,
+            "message": text
+            }
 
     return payload
